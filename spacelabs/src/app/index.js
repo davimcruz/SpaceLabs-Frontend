3f5a7430bd51f430d4f39e6577e77f092b4be055
oDustdev <fab.aquiles123@gmail.com>
2023-08-24 19:33:22 -0300

    .

--- spacelabs/src/app/index.js
+++ spacelabs/src/app/index.js
@@ -5,7 +5,7 @@ export default function Index() {
 
     useEffect(() => {
 
-        $("body").addClass("nk-body bg-lighter npc-general");
+        $("body").addClass("nk-body bg-lighter npc-general dark-mode");
 
     });
 
