1163bad4805677605fe72d8beb58514b4bf45196
oDustdev <fab.aquiles123@gmail.com>
2023-08-24 19:38:58 -0300

    .

--- spacelabs/pages/_app.js
+++ spacelabs/pages/_app.js
@@ -1,7 +1,7 @@
 
 import "../public/assets/css/theme.css"
-import "../public/assets/css/spacelabs.min.css"
 import "../public/assets/css/spacelabs.css"
+import "../public/assets/css/spacelabs.min.css"
 
 import Head from 'next/head'
 import IndexJS from "../src/app";
