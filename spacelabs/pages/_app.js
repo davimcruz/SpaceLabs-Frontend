fe4024853a65476cd4ab8819cf10c162b09d9a32
oDustdev <fab.aquiles123@gmail.com>
2023-08-24 19:44:36 -0300

    .

--- spacelabs/pages/_app.js
+++ spacelabs/pages/_app.js
@@ -1,8 +1,10 @@
 
 import "../public/assets/css/theme.css"
+import "../public/assets/css/spacelabs.rtl.min.css"
 import "../public/assets/css/spacelabs.rtl.css"
-import "../public/assets/css/spacelabs.css"
 import "../public/assets/css/spacelabs.min.css"
+import "../public/assets/css/spacelabs.css"
+
 
 import Head from 'next/head'
 import IndexJS from "../src/app";
@@ -19,9 +21,9 @@ function MyApp({ Component, pageProps }) {
       <meta name="description" content="best platform"/>
       <link rel="shortcut icon" href="/assets/images/spacelabs.png"/>
       <title>SpaceLabs</title>
-      <script src="/assets/js/bundle.js?ver=3.1.0" async />
-      <script src="/assets/js/scripts.js?ver=3.1.0" async />
-      <script src="/assets/js/charts/gd-default.js?ver=3.1.0" async />
+      <script src="/assets/js/bundle.js?ver=3.1.0" defer />
+      <script src="/assets/js/scripts.js?ver=3.1.0" defer />
+      <script src="/assets/js/charts/gd-default.js?ver=3.1.0" defer />
     </Head>
     <Component {...pageProps} />
     </>
