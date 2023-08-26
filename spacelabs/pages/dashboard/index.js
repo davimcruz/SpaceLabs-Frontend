f8e567b1c95bf4dd2dc4b1ae3615117124dbf06d
oDustdev <fab.aquiles123@gmail.com>
2023-08-26 17:37:33 -0300

    .

--- spacelabs/pages/dashboard/index.js
+++ spacelabs/pages/dashboard/index.js
@@ -1,8 +1,7 @@
 import dynamic from 'next/dynamic';
-import DashboardNavbar from '../../src/components/dashboard/Navbar';
-import DashboardWrap from '../../src/components/dashboard/Wrap';
-const Navbar = dynamic(() => import('../../src/components/dashboard/Navbar'), { ssr: false })
-const Wrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false })
+
+const DashboardNavbar = dynamic(() => import('../../src/components/dashboard/Navbar'), { ssr: false })
+const DashboardWrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false })
 
 export default function Dashboard() {
   return (
--- spacelabs/src/app/index.js
+++ spacelabs/src/app/index.js
@@ -30,14 +30,14 @@ export default function Index() {
       "https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"
     );
 
-    document.body.appendChild(bundle);
     document.body.appendChild(scripts);
+    document.body.appendChild(bundle);
     // document.body.appendChild(gdDefault);
     // document.body.appendChild(messages);
 
     return () => {
-      document.body.removeChild(bundle);
       document.body.removeChild(scripts);
+      document.body.removeChild(bundle);
     //   document.body.removeChild(gdDefault);
     //   document.body.removeChild(messages);
     };
