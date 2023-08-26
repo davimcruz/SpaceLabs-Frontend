b536b471c5de23b99b0bb0eec1f44e2daacc5d68
oDustdev <fab.aquiles123@gmail.com>
2023-08-26 15:34:29 -0300

    .

--- spacelabs/pages/_app.js
+++ spacelabs/pages/_app.js
@@ -1,18 +1,22 @@
+import React from "react";
+
 import "../public/assets/css/theme.css";
 import "../public/assets/css/spacelabs.min.css";
 import "../public/assets/css/spacelabs.css";
 
-import Safe from "react-safe"
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import Head from "next/head";
-import Script from "next/script";
 import IndexJS from "../src/app";
 
 function MyApp({ Component, pageProps }) {
-  IndexJS();
+  const router = useRouter();
+  const [Js, setJs] = (useState < React.ReactNode) | (null > null);
 
-  return (
-    <>
+  const handleComplete = () => {
+    setJs(null);
+    setJs(
       <Head>
         <meta name="author" content="SpaceLabs" />
         <meta
@@ -22,18 +26,64 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="best platform" />
         <link rel="shortcut icon" href="/assets/images/spacelabs.png" />
         <title>SpaceLabs</title>
+        <script
+          src="https://spacelabs.vercel.app/assets/js/bundle.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/scripts.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/charts/gd-default.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"
+          async
+        />
       </Head>
+    );
+  };
+
+  useEffect(() => {
+    setJs(
+      <Head>
+        <meta name="author" content="SpaceLabs" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="best platform" />
+        <link rel="shortcut icon" href="/assets/images/spacelabs.png" />
+        <title>SpaceLabs</title>
+        <script
+          src="https://spacelabs.vercel.app/assets/js/bundle.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/scripts.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/charts/gd-default.js?ver=3.1.0"
+          async
+        />
+        <script
+          src="https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"
+          async
+        />
+      </Head>
+    );
+    router.events.on("routeChangeComplete", handleComplete);
+  }, [router.events]);
+
+  IndexJS();
+
+  return (
+    <>
+      {Js}
       <Component {...pageProps} />
-      <Safe.script src="https://spacelabs.vercel.app/assets/js/bundle.js?ver=3.1.0" async />
-      <Safe.script src="https://spacelabs.vercel.app/assets/js/scripts.js?ver=3.1.0" async />
-      <Safe.script
-        src="https://spacelabs.vercel.app/assets/js/charts/gd-default.js?ver=3.1.0"
-        async
-      />
-      <Safe.script
-        src="https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"
-        async
-      />
     </>
   );
 }
