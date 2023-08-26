e6ce0216afa818f6cbd90099a46798f09c58d6f9
oDustdev <fab.aquiles123@gmail.com>
2023-08-26 09:46:11 -0300

    .

--- spacelabs/pages/_app.js
+++ spacelabs/pages/_app.js
@@ -19,10 +19,10 @@ function MyApp({ Component, pageProps }) {
       <meta name="description" content="best platform"/>
       <link rel="shortcut icon" href="/assets/images/spacelabs.png"/>
       <title>SpaceLabs</title>
-      <script src="/assets/js/bundle.js?ver=3.1.0" defer />
-      <script src="/assets/js/scripts.js?ver=3.1.0" defer />
-      <script src="/assets/js/charts/gd-default.js?ver=3.1.0" defer />
-      <script src="/assets/js/apps/messages.js?ver=3.1.0" defer />
+      <script src="/assets/js/bundle.js?ver=3.1.0" async />
+      <script src="/assets/js/scripts.js?ver=3.1.0" async />
+      <script src="/assets/js/charts/gd-default.js?ver=3.1.0" async />
+      <script src="/assets/js/apps/messages.js?ver=3.1.0" async />
 
     </Head>
     <Component {...pageProps} />
