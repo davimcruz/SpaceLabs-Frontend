b05ef06141c9afae49dda89da802a0748b3030ec
oDustdev <fab.aquiles123@gmail.com>
2023-08-26 17:30:02 -0300

    .

--- spacelabs/pages/dashboard/index.js
+++ spacelabs/pages/dashboard/index.js
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic';
 import DashboardNavbar from '../../src/components/dashboard/Navbar';
+import DashboardWrap from '../../src/components/dashboard/Wrap';
 const Navbar = dynamic(() => import('../../src/components/dashboard/Navbar'), { ssr: false })
-const DashboardWrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false })
+const Wrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false })
 
 export default function Dashboard() {
   return (
