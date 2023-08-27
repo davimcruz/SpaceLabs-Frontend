30f1ccb7fcfe8675e06f9c3bd1b1b7e427eed29e
oDustdev <fab.aquiles123@gmail.com>
2023-08-27 11:11:49 -0300

    .

--- spacelabs/src/components/layouts/scripts.js
+++ spacelabs/src/components/layouts/scripts.js
@@ -4,6 +4,7 @@ import Script from 'next/script';
 const LayoutScripts = ({ children }) => {
   return (
     <div>
+      <Script strategy="lazyOnload" src="https://spacelabs.vercel.app/vendors/js/nioapp.min.js?ver=3.1.0"/>
       <Script strategy="lazyOnload" src="https://spacelabs.vercel.app/assets/js/bundle.js?ver=3.1.0"/>
       <Script strategy="lazyOnload" src="https://spacelabs.vercel.app/assets/js/scripts.js?ver=3.1.0"/>      
       <Script strategy="lazyOnload" src="https://spacelabs.vercel.app/assets/js/charts/gd-default.js?ver=3.1.0"/>
