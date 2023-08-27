dfcfa350851cd5fcb97fd8b813b36ded98b0fc41
oDustdev <fab.aquiles123@gmail.com>
2023-08-27 16:02:07 -0300

    .

--- spacelabs/src/app/index.js
+++ spacelabs/src/app/index.js
@@ -3,38 +3,34 @@ import { useEffect } from "react";
 import $ from "jquery";
 
 export default function Index() {
-
   useEffect(() => {
-    $("body").addClass("nk-body bg-lighter npc-general dark-mode");
+    $(function () {
+      $("body").addClass("nk-body bg-lighter npc-general dark-mode");
 
-    var $content = $("[data-content]"),
-      $sidebar = $("." + "nk-sidebar"),
-      $sidebar_body = $("." + "nk-sidebar" + "-body");
+      $("body").on("click", ".nk-nav-compact", function (e) {
+        e.preventDefault();
+        alert("Funcionando");
+        var $self = $(this),
+          get_target = $self.data("target"),
+          $self_content = $("[data-content=" + get_target + "]");
+        $self.toggleClass("compact-active");
+        $self_content.toggleClass("is-compact");
+        if (!$self_content.hasClass("is-compact")) {
+          $self_content.removeClass("has-hover");
+        }
+      });
 
-    $(".nk-nav-compact").on("click", function (e) {
-      e.preventDefault();
-      alert("Funcionando")
-      var $self = $(this),
-        get_target = $self.data("target"),
-        $self_content = $("[data-content=" + get_target + "]");
-      $self.toggleClass("compact-active");
-      $self_content.toggleClass("is-compact");
-      if (!$self_content.hasClass("is-compact")) {
-        $self_content.removeClass("has-hover");
-      }
+      $(".nk-sidebar-body").on("mouseenter", function (e) {
+        if ($sidebar.hasClass("is-compact")) {
+          $sidebar.addClass("has-hover");
+        }
+      });
+
+      $(".nk-sidebar-body").on("mouseleave", function (e) {
+        if ($(".nk-sidebar").hasClass("is-compact")) {
+          $(".nk-sidebar").removeClass("has-hover");
+        }
+      });
     });
-
-    $sidebar_body.on("mouseenter", function (e) {
-      if ($sidebar.hasClass("is-compact")) {
-        $sidebar.addClass("has-hover");
-      }
-    });
-
-    $sidebar_body.on("mouseleave", function (e) {
-      if ($sidebar.hasClass("is-compact")) {
-        $sidebar.removeClass("has-hover");
-      }
-    });
-
   });
 }
