260e0c1608939c59479ac5833cb1566ab5a751bc
oDustdev <fab.aquiles123@gmail.com>
2023-08-27 12:45:09 -0300

    .

--- spacelabs/src/components/layouts/scripts.js
+++ spacelabs/src/components/layouts/scripts.js
@@ -4,11 +4,11 @@ import Script from 'next/script';
 const LayoutScripts = ({ children }) => {
   return (
     <div>
-      {/* <Script src="https://spacelabs.vercel.app/vendors/js/nioapp.min.js?ver=3.1.0"/> */}
+      {/* <Script src="https://spacelabs.vercel.app/vendors/js/nioapp.min.js?ver=3.1.0"/>
       <Script src="https://spacelabs.vercel.app/assets/js/bundle.js?ver=3.1.0"/>
       <Script src="https://spacelabs.vercel.app/assets/js/scripts.js?ver=3.1.0"/>      
       <Script src="https://spacelabs.vercel.app/assets/js/charts/gd-default.js?ver=3.1.0"/>
-      <Script src="https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"/>
+      <Script src="https://spacelabs.vercel.app/assets/js/apps/messages.js?ver=3.1.0"/> */}
       {children}
     </div>
   );
