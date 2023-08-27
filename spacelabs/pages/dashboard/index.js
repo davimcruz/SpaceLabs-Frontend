459fdc7a6e7df5fb0b23da6b98ebb9249d1b7025
Davi <davimachadocruz@gmail.com>
2023-08-27 14:57:33 -0300

    add overview route

--- spacelabs/pages/dashboard/index.js
+++ spacelabs/pages/dashboard/index.js
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
-import LayoutScripts from '../../src/components/layouts/scripts'
+import LayoutScripts from '../../src/components/layouts/scripts';
 
-const DashboardNavbar = dynamic(() => import('../../src/components/dashboard/Navbar'), { ssr: false })
-const DashboardWrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false })
+const DashboardNavbar = dynamic(() => import('../../src/components/dashboard/Navbar'), { ssr: false });
+const DashboardWrap = dynamic(() => import('../../src/components/dashboard/Wrap'), { ssr: false });
 
 export default function Dashboard() {
+  const router = useRouter();
+
+    useEffect(() => {
+      // Redirecionar para /dashboard/overview
+      router.push('/dashboard/overview');
+    }, 
+  );
+
   return (
     <LayoutScripts>
       <div className="nk-app-root">
