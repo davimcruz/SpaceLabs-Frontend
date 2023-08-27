5763fcae01da2024f7deb961a3b27aebf0ea6da6
oDustdev <fab.aquiles123@gmail.com>
2023-08-27 15:38:15 -0300

    .

--- spacelabs/pages/dashboard/index.js
+++ spacelabs/pages/dashboard/index.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import LayoutScripts from '../../src/components/layouts/scripts';
-=======
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
->>>>>>> 6af7ca888386a9936cb5cb3229022adecd06c038
 
 export const getServerSideProps = ({req, res}) => {
   return {
@@ -15,18 +10,10 @@ export const getServerSideProps = ({req, res}) => {
   }
 }
 
-<<<<<<< HEAD
 export default function Dashboard() {
 
   return (
     <LayoutScripts>
     </LayoutScripts>
-=======
-  useEffect(() => {
-    router.push('/dashboard/overview');
-    },
->>>>>>> 6af7ca888386a9936cb5cb3229022adecd06c038
   );
 }
-
-
