85c3f32f6d2a8bac2e4255a4d67e2a33426bde80
Davi <davimachadocruz@gmail.com>
2023-08-27 14:48:57 -0300

    add overview route

--- spacelabs/pages/dashboard/overview/index.js
+++ spacelabs/pages/dashboard/overview/index.js
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import LayoutScripts from '../../src/components/layouts/scripts'
+import LayoutScripts from '../../../src/components/layouts/scripts';
 
 const DashboardNavbar = dynamic(() => import('../../../src/components/dashboard/Navbar'), { ssr: false })
 const DashboardWrap = dynamic(() => import('../../../components/dashboard/Wrap'), { ssr: false })
