14b6f5abd0e5fb4f4ef84192fda3e60111eafb7b
Davi <davimachadocruz@gmail.com>
2023-08-28 15:22:09 -0300

    Redirecionamento principal para Overview

--- spacelabs/pages/index.js
+++ spacelabs/pages/index.js
@@ -1,35 +1,16 @@
-import { useRouter } from "next/router";
-import LayoutScripts from "../src/components/layouts/scripts";
+export const getServerSideProps = ({req, res}) => {
+  return {
+    redirect: {
+        permanent: false,
+        destination: "/dashboard/overview"
+    },
+    props: {}
+  }
+}
 
-export default function NotFound() {
-  const router = useRouter();
+export default function Dashboard() {
+
   return (
-    <LayoutScripts>
-      <div class="nk-app-root">
-        <div class="nk-main ">
-            <div class="nk-wrap nk-wrap-nosidebar">
-                <div class="nk-content ">
-                    <div class="nk-block nk-block-middle wide-xs mx-auto">
-                        <div class="nk-block-content nk-error-ld text-center">
-                            <h2 class="nk-coming-head">Coming Soon!</h2>
-                            <h3 class="nk-error-title">Enjoy the best, with the best.</h3>
-                            <p class="nk-error-text">Experience something totally new and out of the ordinary. News soon, keep up with us!</p>
-                            <a
-                              href="#"
-                              className="btn btn-lg btn-primary btn-dim mt-2"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                router.push("/dashboard/overview");
-                              }}
-                            >
-                              Back To Platform
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </LayoutScripts>
+  <></>
   );
 }
