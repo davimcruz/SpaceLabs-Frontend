dd9037430e5e55e66eee321b508775ba5b86a5c9
Davi <davimachadocruz@gmail.com>
2023-08-28 15:32:21 -0300

    Erros folder add

--- spacelabs/pages/_error.js
+++ spacelabs/pages/_error.js
@@ -1,47 +1,13 @@
-import { useRouter } from "next/router";
-import LayoutScripts from "../src/components/layouts/scripts";
-
-export default function NotFound() {
-  const router = useRouter();
+function CustomErrorPage({ statusCode }) {
   return (
-    <LayoutScripts>
-      <div className="nk-app-root">
-        <div className="nk-main ">
-          <div className="nk-wrap nk-wrap-nosidebar">
-            <div className="nk-content ">
-              <div className="nk-block nk-block-middle wide-md mx-auto">
-                <div className="nk-block-content nk-error-ld text-center">
-                  <img
-                    className="nk-error-gfx"
-                    src="/assets/images/gfx/error-404.svg"
-                    alt=""
-                  />
-                  <div className="wide-xs mx-auto">
-                    <h3 className="nk-error-title">
-                      Oops! Why did you come here?
-                    </h3>
-                    <p className="nk-error-text">
-                      We are sorry for the incident. You are trying to access a
-                      non-existent or deleted page, please return to our
-                      platform.
-                    </p>
-                    <a
-                      href="#"
-                      className="btn btn-lg btn-primary btn-dim mt-2"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        router.push("/dashboard");
-                      }}
-                    >
-                      Back To Platform
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </LayoutScripts>
+    <div>
+      {statusCode ? (
+        <p>Ocorreu um erro no servidor: {statusCode}</p>
+      ) : (
+        <p>Ocorreu um erro na página</p>
+      )}
+    </div>
   );
 }
+
+export default CustomErrorPage;
---
+++ spacelabs/pages/errors/404.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import LayoutScripts from "../../src/components/layouts/scripts";
+
+function NotFoundPage() {
+  return (
+    <LayoutScripts>
+      <div className="nk-app-root">
+        <div className="nk-main ">
+          <div className="nk-wrap nk-wrap-nosidebar">
+            <div className="nk-content ">
+              <div className="nk-block nk-block-middle wide-md mx-auto">
+                <div className="nk-block-content nk-error-ld text-center">
+                  <Image
+                    className="nk-error-gfx"
+                    src="/assets/images/gfx/error-404.svg"
+                    alt=""
+                  />
+                  <div className="wide-xs mx-auto">
+                    <h3 className="nk-error-title">
+                      Oops! Why did you come here?
+                    </h3>
+                    <p className="nk-error-text">
+                      We are sorry for the incident. You are trying to access a
+                      non-existent or deleted page, please return to our
+                      platform.
+                    </p>
+                    <a
+                      href="#"
+                      className="btn btn-lg btn-primary btn-dim mt-2"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        router.push("/dashboard/overview");
+                      }}
+                    >
+                      Back To Platform
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutScripts>
+  );
+}
+
+export default NotFoundPage;
